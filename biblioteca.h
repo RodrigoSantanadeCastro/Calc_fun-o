270bfaae9c50d38fbd1100ef94cd235dde126431
rodrigosantana <rodrigo.santana58@etec.sp.gov.br>
2023-10-25 21:21:15 -0300

    Termino_BB

--- biblioteca.h
+++ biblioteca.h
@@ -1,5 +1,26 @@
-void soma(int a,int b){
-  int total;
+void soma(float a,float b){
+  float total;
   total = a+b;
-  printf("Resultado:%d",total);
+  printf("Resultado:%f",total);
+}
+void subritacao(float a, float b){
+  float total;
+  total = a-b;
+  printf("Resultado:%f",total);
+}
+void multiplicacao(float a, float b){
+  float total;
+  total = a*b;
+  printf("Resultado:%f",total);
+}
+void divisao(float a, float b){
+  float total;
+  if (b==0){
+  printf("Divisao por Zero");
+  }else{
+  total = a/b;
+  printf("Resultado:%f",total);
+  }
+
+	
 }
